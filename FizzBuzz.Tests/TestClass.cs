aaaaeab13d31bca00e628c0e9212ce6ff386331c
connors1990 <connors1990@gmail.com>
2014-09-10 19:22:17 +0200

    adding tests

---
+++ FizzBuzz.Tests/TestClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzz.Tests
+{
+    public class TestClass
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
